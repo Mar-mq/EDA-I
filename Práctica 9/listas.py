94675f701796c4470c54453c8df474a5e05595bb
Mar-mq <mq07014@gmail.com>
2025-12-11 09:48:27 -0600

    Create listas.py with month days data
    Initialize lists for days in each month and print values.

---
+++ Práctica 9/listas.py
@@ -0,0 +1,14 @@
+lista_diasDelMes=[31,28,31,30,31,30,31,31,30,31,30,31]
+print (lista_diasDelMes)
+print (lista_diasDelMes[0])
+print (lista_diasDelMes[5])
+print (lista_diasDelMes[-1])
+listaDiasMeses=[['enero',31], ['febrero',28], ['marzo',31], ['abril',30], ['mayo',31], ['junio',30], ['julio',31], ['agosto',31], ['septiembre',30], ['octubre',31], ['noviembre',30], ['diciembre',31]]
+print (listaDiasMeses)
+print (listaDiasMeses[0])
+print (listaDiasMeses[5][0])
+print (listaDiasMeses[0][1])
+print (listaDiasMeses[2][0])
+print (listaDiasMeses[3][1])
+lista_diasDelMes[1] = 29
+print (lista_diasDelMes)
