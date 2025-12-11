a9d448ec853d95db102b33c1384f271991395dd4
Mar-mq <mq07014@gmail.com>
2025-12-11 09:47:17 -0600

    Create hola_mundo.py

---
+++ Práctica 9/hola_mundo.py
@@ -0,0 +1,23 @@
+x = "Hola Mundo"
+cadena = 18
+print(type(x))
+print(type(cadena))
+SEGUNDOS_POR_DIA = 60 * 60 * 24
+PI = 3.14
+cadena1 = 'Hola '
+cadena2 = 'Mundo'
+print(cadena1)
+print(cadena2)
+concat_cadenas = cadena1 + cadena2
+print(concat_cadenas)
+num_cadena = "{} {}".format("Hola", "Mundo")
+print(num_cadena)
+cadena1 = 'Hola'
+cadena2 = 'Mundo'
+num_cadena = "{1} {0}".format(cadena1, cadena2)
+print(num_cadena)
+cadena1 = 'uno'
+cadena2 = 'dos'
+cadena3 = 'tres'
+num_cadena = "{2} {1} {0}".format(cadena1, cadena2, cadena3)
+print(num_cadena)
